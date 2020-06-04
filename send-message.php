f947531dbcd04baba42042b20bd0c889a3cb7fad
root <root@DESKTOP-07OK0S8.localdomain>
2020-06-04 22:34:33 +0700

    send command

---
+++ send-message.php
@@ -0,0 +1,17 @@
+<?php
+set_time_limit(0);
+date_default_timezone_set('Asia/Bangkok');
+require_once('bot.php');
+
+$bot = new bot($_POST);
+if ($bot->command['action'] == 'checkbot') {
+   $bot->checkbot();
+   exit;
+} else {
+   $bot->savelog('Bot Start with runcount : '.$bot->runCount);
+   while ($bot->login()) {
+      $bot->work();
+      // $bot->get_newprofile();
+   }
+}
+?>
